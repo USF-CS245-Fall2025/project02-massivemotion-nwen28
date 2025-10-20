3ecc6d16e0757684a568f7a4695376c5ae7781f6
Nicholas Wen <nwen@dons.usfca.edu>
2025-10-19 18:00:03 -0700

    comments

--- src/MassiveMotion.java
+++ src/MassiveMotion.java
@@ -17,20 +17,20 @@ public class MassiveMotion extends JPanel implements ActionListener {
     int bodyVelocity;
     int bodySize;
 
-
+    //Initializes unspecified list
     List<CelestialBody> celestialBodies;
 
-    // TODO: Consider removing the next two lines (coordinates for two balls)
-    protected int x1, y1;
-    protected int x2, y2;
-
-
-    // public MassiveMotion(String propfile) {
+    /** MassiveMotion method opens, loads, and reads the property file
+     * and creates the star object from the file and determines what type of list
+     * celestialBodies is
+     * @param propfile takes in the name of the property file
+     */
     public MassiveMotion(String propfile) {
         // TODO: insert your code to read from configuration file here.
 
         Properties prop = new Properties();
 
+        //Opens property file and loads it
         try{
             FileInputStream filename = new FileInputStream(propfile);
             prop.load(filename);
@@ -39,6 +39,7 @@ public class MassiveMotion extends JPanel implements ActionListener {
             System.out.println("File Not Found");
         }
 
+        //Saves all the following information after being read from the property file
         int delay = Integer.parseInt(prop.getProperty("timer_delay"));
 
         windowSizeX = Integer.parseInt(prop.getProperty("window_size_x"));
@@ -59,6 +60,8 @@ public class MassiveMotion extends JPanel implements ActionListener {
         String listType = prop.getProperty("list");
         System.out.println("Using a: " + listType);
 
+        //Checks what type of list the property file specifies and changes the list
+        // initialized at the top to the now specified type of list
         if(listType.equalsIgnoreCase("arraylist")){
             celestialBodies = new ArrayList<>();
         } else if(listType.equalsIgnoreCase("single")){
@@ -69,6 +72,7 @@ public class MassiveMotion extends JPanel implements ActionListener {
             celestialBodies = new DummyHeadLinkedList<>();
         }
 
+        //Creates the star that was saved in the property file and adds it to the list of celestialBodies
         CelestialBody star = new CelestialBody(starPosX, starPosY, starVelocityX, star_velocityY, starSize, Color.RED);
         celestialBodies.add(star);
 
@@ -76,21 +80,17 @@ public class MassiveMotion extends JPanel implements ActionListener {
         //Delay used to be 75 as default
         tm = new Timer(delay, this); // TODO: Replace the first argument with delay with value from config file.
 
-        // TODO: Consider removing the next two lines (coordinates) for random starting locations.
-        //x1 = 100; y1 = 50;
-        //x2 = 200; y2 = 400;
     }
 
+    /** paintComponent method iterates through list of celestial objects
+     *  and draws them on the canvas with the given information
+     * @param graphics 
+     */
     public void paintComponent(Graphics g) {
         super.paintComponent(g); // Probably best you leave this as is.
 
-        // TODO: Paint each ball. Here's how to paint two balls, one after the other:
-        //g.setColor(Color.BLUE);
-        //g.fillOval(x1, y1, 20, 20);
-
-        //g.setColor(Color.RED);
-        //g.fillOval(x2, y2, 20, 20);
-
+        //Iterates through the list of celestial bodies and creates a celestia object with 
+        // the given parameters and draws it on the canvas
         for(int i = 0; i < celestialBodies.size(); i++){
             g.setColor(celestialBodies.get(i).getColor());
             g.fillOval(celestialBodies.get(i).getX(), celestialBodies.get(i).getY(), celestialBodies.get(i).getSize(), celestialBodies.get(i).getSize());
@@ -101,13 +101,26 @@ public class MassiveMotion extends JPanel implements ActionListener {
     }
 
 
+    /** actionPerformed method uses the probability of a new celestial 
+     * object being created and if it is created, adds the obect
+     * to the celestialBodies list
+     * @param actionEvent
+     */
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
 
+        //Generates a random number and see if it falls within the genX chance of spawning
         if(Math.random() <= genX){
+
+            //Boolean that keeps track of whether the object is going to spawn
+            // on the top or bottom
             boolean spawnLocation;
+
+            //Random x coordinate along the window size
             int x = (int)(Math.random() * windowSizeX);
             int y;
+
+            //50/50 chance of creating the celesial object on the top or bottom of the screen
             if(Math.random() <= 0.5){
                 spawnLocation = true;
             } else{
@@ -120,9 +133,12 @@ public class MassiveMotion extends JPanel implements ActionListener {
                 y = windowSizeY;
             }
 
+            //Sets the velocity at 0 at the start
             int velX = 0;
             int velY = 0;
 
+            //Keeps generating a velocity from the given information in the property file
+            // until the velocity generated isn't equal to 0
             while(velX == 0){
                 velX = (int)(Math.random() * (bodyVelocity - (-bodyVelocity) + 1)) + (-bodyVelocity);
             }
@@ -131,14 +147,20 @@ public class MassiveMotion extends JPanel implements ActionListener {
                 velY = (int)(Math.random() * (bodyVelocity - (-bodyVelocity) + 1)) + (-bodyVelocity);
             }
             
+            //Adds the newly created celestial object to the list of celestial bodies
             CelestialBody newBody = new CelestialBody(x, y, velX, velY, bodySize, Color.BLACK);
             celestialBodies.add(newBody);
         }
 
+        //Generates a random number to see if the celestial object is created
         if(Math.random() <= genY){
             boolean spawnLocation;
+
+            //Genereates a random y coordinate along the window size to spawn the object
             int y = (int)(Math.random() * windowSizeY);
             int x;
+
+            //50/50 chance to create the object on the left or right side of the screen
             if(Math.random() <= 0.5){
                 spawnLocation = true;
             } else{
@@ -166,11 +188,15 @@ public class MassiveMotion extends JPanel implements ActionListener {
             celestialBodies.add(newBody);
         }
 
+        //Iterates backwards and changes the coordinates of the celestial body
+        // to simulate the object "moving"
         for(int i = celestialBodies.size()-1; i >= 0; i--){
             CelestialBody body = celestialBodies.get(i);
             body.x += celestialBodies.get(i).getVelX();
             body.y += celestialBodies.get(i).getVelY();
 
+            //Checks if the object has moved out of the bounds of the window and removes the object
+            // from the celestialBodies list if it's true
             if(body.getX() > windowSizeX || body.getX() < 0 || body.getY() > windowSizeY || body.getY() < 0){
                 celestialBodies.remove(i);
             }
@@ -185,7 +211,6 @@ public class MassiveMotion extends JPanel implements ActionListener {
         System.out.println("Massive Motion starting...");
         
         MassiveMotion mm = new MassiveMotion(args[0]);
-        //MassiveMotion mm = new MassiveMotion();
 
         JFrame jf = new JFrame();
         jf.setTitle("Massive Motion");
@@ -195,6 +220,3 @@ public class MassiveMotion extends JPanel implements ActionListener {
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
-
-
-//test to see if this works
